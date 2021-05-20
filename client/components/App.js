7880e1a22ca2a73b300dd50fd3a49636e19b8b45
Muhammad Haseeb <haseeb.lums@gmail.com>
2021-05-20 17:52:47 +0500

    preparing demo

--- client/components/App.js
+++ client/components/App.js
@@ -1,15 +1,21 @@
 import React, { Component } from 'react';
+import Dropdown from 'react-dropdown';
+import 'react-dropdown/style.css';
 import '../css/App.css';
 class App extends Component {
   render() {
+    const options = [
+      {value: 1, className: "image1"},
+      {value: 2, className: "image2"},
+      {value: 3, className: "image3"}
+    ];
+    const defaultOption = "Choose Room";
+
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <div className="SideBySide">
+          <Dropdown options={options} onChange={this._onSelect} value={defaultOption} placeholder="Select an option" />
+        </div>
       </div>
     );
   }
